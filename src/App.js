fdc0b7729363166947c2a2ed941fbddf489dd17d
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-21 02:47:49 +0100

    handled question changing on button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Question from './components/Question'
 import './App.css';
 import Answer from './components/Answer';
@@ -11,25 +11,71 @@ const QUIZ = [{
     {answer:"<js>", correct:false},
     {answer:"<source>", correct:false}
   ]
+},
+{
+  question:"EBEBEB?",
+  answers: [
+    {answer:"<script>", correct:true},
+    {answer:"<link>", correct:false},
+    {answer:"<js>", correct:false},
+    {answer:"<source>", correct:false}
+  ]
+},
+{
+  question:"asddjkas?",
+  answers: [
+    {answer:"<script>", correct:true},
+    {answer:"<link>", correct:false},
+    {answer:"<js>", correct:false},
+    {answer:"<source>", correct:false}
+  ]
+},
+{
+  question:"DKjdl;AKD?",
+  answers: [
+    {answer:"<script>", correct:true},
+    {answer:"<link>", correct:false},
+    {answer:"<js>", correct:false},
+    {answer:"<source>", correct:false}
+  ]
 }
 ] 
 
 let FIRST_QUESTION = QUIZ[0]
-
+let arrIndex=0;
 function App() {
+const [quizQuestion, setQuizQuestion] = useState(QUIZ[0]);
+const [questionNumber, setQuestionNumber] = useState(0)
+function handleClick(event) {
+  
+  let name = event.currentTarget.name
+  console.log("CLICK", name)
+}
+
+function handleSubmit(e) {
+  
+if(QUIZ.length> questionNumber) {
+  setQuestionNumber(previousValue => previousValue+1)
+  setQuizQuestion(QUIZ[questionNumber])
+
+}
+
+}
+
   return (
     <div className="mainDiv">
      <div className="quizDiv">
        <div className="question">
-       <Question question={FIRST_QUESTION.question}/>
+       <Question question={quizQuestion.question}/>
        </div>
       
        <div className="answers">
-         {FIRST_QUESTION.answers.map((answer, index)=>{
-           return <Answer key={index} answer={answer} />
+         {quizQuestion.answers.map((answer, index)=>{
+           return <Answer key={index} answer={answer} handleClick={handleClick} index={index}/>
          })}
        </div>
      </div>
+     <button onClick={handleSubmit}>NEXT</button>
     </div>
   );
 }
--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Answer({answer}) {
+function Answer({answer, handleClick, index}) {
 
     return(
-        <div style={{width:"calc(50% - 2px)", border:"1px solid blue", height:"calc(50% - 2px)", position:"relative"}}>
+        <div style={{width:"calc(50% - 2px)", border:"1px solid blue", height:"calc(50% - 2px)", position:"relative"}} name={`asnwer${index+1}`}onClick={handleClick}>
             <p style={{position:"absolute", top:"50%", left:"50%",transform:"translate(-50%, -50%)", margin:"0"}}>{answer.answer}</p>
         </div>
     )
